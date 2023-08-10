c9511ee339b94ce87e95327afd51c9b7f3dbf730
bmalkes <bmalkes@gmail.com>
2023-08-10 20:33:05 +0300

    feat:draggable

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,18 +1,30 @@
 import classNames from "classnames";
 import "./Task.css";
 import { useStore } from "../store";
+import { BsTrash } from "react-icons/bs";
 
 // eslint-disable-next-line react/prop-types
 export const Task = ({ title }) => {
   const task = useStore((store) =>
     store.tasks.find((task) => task.title === title)
   );
-  console.log(task.title);
+  const deleteTask = useStore((store) => store.deleteTask);
+  const setDraggedTask = useStore((store) => store.setDraggedTask);
+
   return (
-    <div className="task">
+    <div
+      className="task"
+      draggable
+      onDragStart={() => setDraggedTask(task.title)}
+    >
       <div>{task.title}</div>
       <div className="bottomWrapper">
-        <div></div>
+        <div>
+          <BsTrash
+            onClick={() => deleteTask(task.title)}
+            style={{ cursor: "pointer" }}
+          />
+        </div>
         <div className={classNames("status", task.state)}>{task.state}</div>
       </div>
     </div>
--- src/store.js
+++ src/store.js
@@ -1,7 +1,41 @@
 import { create } from "zustand";
+import { devtools, persist, subscribeWithSelector } from "zustand/middleware";
 // eslint-disable-next-line no-unused-vars
 const store = (set) => ({
   tasks: [{ title: "TestTask", state: "DONE" }],
+  addTask: (title, state) =>
+    set(
+      (store) => ({ tasks: [...store.tasks, { title, state }] }),
+      false,
+      "addTask"
+    ),
+  deleteTask: (title) =>
+    set(
+      (store) => ({
+        tasks: store.tasks.filter((task) => task.title !== title),
+      }),
+      false,
+      "deleteTask"
+    ),
+  setDraggedTask: (title) => set({ draggedTask: title }),
+  moveTask: (title, state) =>
+    set((store) => ({
+      tasks: store.tasks.map((task) =>
+        task.title === title ? { title, state } : task
+      ),
+    })),
 });
 
-export const useStore = create(store);
+const log = (config) => (set, get, api) =>
+  config(
+    (...args) => {
+      console.log(args);
+      set(...args);
+    },
+    get,
+    api
+  );
+
+export const useStore = create(
+  subscribeWithSelector(log(persist(devtools(store), { name: "store" })))
+);
